f1fc9f2bf1fff16e564be66d6aa2c17b50e3dcf6
vaidiksule <vaidiksulemusic@gmail.com>
2025-08-07 02:23:58 +0530

    Made Chnages for Vercel and Django deployement

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-cbrqz5yy2pbx#t13-onfq(9m3kfyd=*wk2=4y+06ao=*vy!@&k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["getsentimate-backend.onrender.com", "localhost", "127.0.0.1"]
+
 
 
 # Application definition
@@ -77,8 +78,7 @@ TEMPLATES = [
 
 # Allow frontend origin
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://localhost:3000",
+    os.getenv("CORS_ALLOWED_ORIGINS"),
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -138,3 +138,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
