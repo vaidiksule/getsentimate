fae6093a31382d5bac8e3e25d3f01a730e760f2a
vaidiksule <vaidiksulemusic@gmail.com>
2025-08-14 15:27:12 +0530

    updated setting.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -130,9 +130,9 @@ CORS_ALLOW_CREDENTIALS = True
 
 # REST Framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
